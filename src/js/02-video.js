dca90242edea7b4af6eebbe9b620118e21bf3e30
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-05-31 23:07:10 +0300

    50%_second_task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+// import throttle from ('lodash.throttle');
 
+const player = new Player('vimeo-player', {
+  id: 19231868,
+  width: 640,
+});
+
+player.on('timeupdate', function (data) {
+    console.log('timeupdate:', data);
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+});
+
+var currentTime = localStorage.getItem('videoplayer-current-time')
+if (currentTime) {
+    player.setCurrentTime(currentTime);
+}
+
+
+// var throttled = _.throttle(renewToken, 300000, { 'trailing': false });
