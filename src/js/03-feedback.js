dc0f67f81e479b22cc45c9bf9425fd971aed16e8
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-06-04 11:24:42 +0300

    changes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -13,19 +13,25 @@ emailFeedback.addEventListener('input', throttle(onEmailInput, 500));
 
 function onFormSubmit(e) {
   e.preventDefault();
-  e.target.reset();
+  if (textareaFeedback.value === '' || emailFeedback.value === '') {
+    alert('Всі поля повинні бути заповнені!!!');
+  } else {
+    console.log('email: ', localStorage.getItem('feedback-form-state-email'));
+    console.log(
+      'message: ',
+      localStorage.getItem('feedback-form-state-textarea')
+    );
 
-  localStorage.removeItem('feedback-form-state-textarea');
-  localStorage.removeItem('feedback-form-state-email');
+    localStorage.removeItem('feedback-form-state-textarea');
+    localStorage.removeItem('feedback-form-state-email');
 
-//   console.log('email: ', messageEmail);
-//   console.log('message: ', messageTextarea);
+    e.target.reset();
+  } return false;
 }
 
 function onTextareaInput(e) {
   const messageTextarea = e.target.value;
-    localStorage.setItem('feedback-form-state-textarea', messageTextarea);
-    console.log(messageTextarea)
+  localStorage.setItem('feedback-form-state-textarea', messageTextarea);
 }
 
 function onEmailInput(e) {
